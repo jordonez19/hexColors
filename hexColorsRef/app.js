51f7e12fccb9c56bd7743ea7c9fd8d79a4d6b8f8
jordonez19 <64795542+jordonez19@users.noreply.github.com>
2020-07-22 11:48:34 -0500

    Add files via upload

---
+++ hexColorsRef/app.js
@@ -0,0 +1,18 @@
+const hexNumbers =[0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+const hexBtn = document.querySelector('.hexBtn');
+const bodybg = document.querySelector('body');
+const hex= document.querySelector('.hex');
+
+hexBtn.addEventListener('click.',getHex);
+
+function getHex(){
+let hexCol = '#';
+
+for(let i = 0; i<6;i++){
+let random= math.floor(math.random()*hexNumbers.length);
+hexCol +=hexNumbers[random];
+
+}
+bodybg.style.backgroundColor= hexCol;
+hex.innerHTML = hexCol;
+}
